119c7a43f4274c5e310cf6d2638476c9f0323ad7
kilfour <kilfour@gmail.com>
2025-08-28 09:12:24 +0200

    mvc skeleton added

---
+++ HorsesForCourses.MVC/Controllers/CoachesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorsesForCourses.MVC.Controllers;
+
+// public class CoachesController(CoachesRepository repository) : Controller
+// {
+//     private readonly CoachesRepository repository = repository;
+
+//     [HttpGet]
+//     public async Task<IActionResult> Index(int page = 1, int pageSize = 25)
+//         => View(await repository.GetTheCoachSummaries.All(new PageRequest(page, pageSize)));
+// }
