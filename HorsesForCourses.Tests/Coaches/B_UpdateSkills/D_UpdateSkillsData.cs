067a8ac72c738c4f00f93e3cac7f6cecb9b6d7bb
kilfour <kilfour@gmail.com>
2025-08-30 23:14:40 +0200

    moved

--- HorsesForCourses.Tests/Coaches/B_UpdateSkills/D_UpdateSkillsData.cs
+++ HorsesForCourses.Tests/Coaches/B_UpdateSkills/D_UpdateSkillsData.cs
@@ -14,15 +14,15 @@ public class D_UpdateSkillsData : DatabaseTests
         AddToDb(coach);
     }
 
+    private Coach Reload() => Reload(GetDbContext());
+    private Coach Reload(AppDbContext dbContext) => dbContext.Coaches.Single(a => a.Id == coach.Id);
+
     private void Act()
     {
         var context = GetDbContext();
         Reload(context).UpdateSkills(TheCanonical.Skills);
         context.SaveChanges();
     }
-
-    private Coach Reload() => Reload(GetDbContext());
-    private Coach Reload(AppDbContext dbContext) => dbContext.Coaches.Single(a => a.Id == coach.Id);
 
     [Fact]
     public void Skills_can_be_updated()
